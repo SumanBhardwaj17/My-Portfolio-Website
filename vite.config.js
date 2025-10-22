24dbdaed7b7aeff16c41da6c0c239ab778805b2f
SumanBhardwaj17 <suman1722005@gmail.com>
2025-10-22 20:11:02 +0530

    Prepare for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,8 @@ export default defineConfig({
   plugins: [
     react(),
   ],
-  // Use the repo-base only in production so `npm run dev` works locally
-  // base: process.env.NODE_ENV === "production" ? "/My-Portfolio-Website/" : "./",   // base: './',
-  base: "/My-Portfolio-Website/",
+  // Use repo name only for production builds
+  base: process.env.NODE_ENV === "production" ? "/My-Portfolio-Website/" : "/",
   resolve: {
     alias: {
       "@": "/src", // optional: lets you import like "@/components/Component"
